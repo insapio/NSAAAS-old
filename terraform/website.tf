29af1a338af984487beb080d500bc597ac1828ee
Alexander Templeton <blakmatr@gmail.com>
2019-09-30 20:18:08 -0500

    adding asset push

--- terraform/website.tf
+++ terraform/website.tf
@@ -2,6 +2,39 @@
 # Website #
 ###########
 
+resource "aws_s3_bucket_object" "site_assets" {
+  bucket = var.site_domain
+  key    = "manifest.json"
+  source = "../public"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = "${filemd5("../public.manifest.json")}"
+}
+
+resource "aws_s3_bucket_object" "site_assets" {
+  bucket = var.site_domain
+  key    = "index.html"
+  source = "../public"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = "${filemd5("../public/index.html")}"
+}
+
+resource "aws_s3_bucket_object" "site_assets" {
+  bucket = var.site_domain
+  key    = "favicon.ico"
+  source = "../public"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = "${filemd5("../public/favicon.ico")}"
+}
+
 resource "aws_s3_bucket" "website" {
   bucket = var.site_domain
 
