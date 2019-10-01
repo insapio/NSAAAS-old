34629d0d9e1a4b8e34536b11134de5cbd8c8162e
Alexander Templeton <blakmatr@gmail.com>
2019-09-30 20:21:32 -0500

    path typo

--- terraform/website.tf
+++ terraform/website.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_object" "site_asset_manifest" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = "${filemd5("../public.manifest.json")}"
+  etag = "${filemd5("../public/manifest.json")}"
 }
 
 resource "aws_s3_bucket_object" "site_asset_index" {
