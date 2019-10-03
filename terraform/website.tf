6cd53e24f8d7650ee64b420342b46a1bf922680c
Alexander Templeton <blakmatr@gmail.com>
2019-10-02 19:45:21 -0500

    updating content type

--- terraform/website.tf
+++ terraform/website.tf
@@ -3,9 +3,10 @@
 ###########
 
 resource "aws_s3_bucket_object" "site_asset_manifest" {
-  bucket = var.site_domain
-  key    = "manifest.json"
-  source = "../public/manifest.json"
+  bucket           = var.site_domain
+  key              = "manifest.json"
+  source           = "../public/manifest.json"
+  content_encoding = "application/json"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -14,9 +15,11 @@ resource "aws_s3_bucket_object" "site_asset_manifest" {
 }
 
 resource "aws_s3_bucket_object" "site_asset_index" {
-  bucket = var.site_domain
-  key    = "index.html"
-  source = "../public/index.html"
+  bucket           = var.site_domain
+  key              = "index.html"
+  source           = "../public/index.html"
+  content_encoding = "text/html"
+
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
